d25a04100afaa5d184e29846a5bae08b2b74c448
Sagar Pandya <sarg@outlook.com>
2016-09-23 20:52:18 +0000

    000

---
+++ getth.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+	s.name = 'getth'
+	s.version = '0.0.0'
+	s.date = '2016-08-27'
+	s.summary = 'Getth'
+	s.description = 'Use English-words to access elements within strings and arrays: second, third, fourth upto tenth'
+  s.authors = ["Sagar Pandya"]
+  s.email = 'sarg@outlook.com'
+  s.files = ["lib/getth.rb"]
+  s.homepage = 'http://rubygems.org/gems/getth'
+  s.license = 'Beerware'
+end
---
+++ lib/getth.rb
@@ -0,0 +1,57 @@
+module Getth
+	def second
+		self[1]
+	end
+
+	def third
+		self[2]
+	end
+	
+	def fourth
+		self[3]
+	end
+	
+	def fifth
+		self[4]
+	end
+
+	def sixth
+		self[5]
+	end
+
+	def seventh
+		self[6]
+	end
+
+	def eighth
+		self[7]
+	end
+
+	def nineth
+		self[8]
+	end
+
+	def tenth
+		self[9]
+	end
+end
+
+class Array
+	include Getth
+end
+
+class String
+	include Getth
+end
+
+=begin
+
+###########Example#############
+arr = (1..10).to_a.shuffle => [9, 2, 7, 10, 3, 8, 4, 1, 5, 6]
+arr.second     =>  2
+arr.third      =>  7
+arr.fourth     => 10
+arr.tenth      =>  6
+"Hello".second => "e"
+
+=end
