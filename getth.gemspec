1f09bc1520e5752f6cc9d8d0668a0e92317313b0
Sagar Pandya <sarg@outlook.com>
2017-01-19 20:50:30 +0000

    004 Refactor tests; update readme

--- getth.gemspec
+++ getth.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
 	s.name = 'getth'
-	s.version = '0.0.3'
+	s.version = '0.0.4'
 	s.add_runtime_dependency "ordinalize", [">= 0"]
 	s.date = '2017-01-19'
 	s.summary = 'Getth'
-	s.description = 'This gem defines methods _first, second, third ... ninth, tenth_ for retrieving values from strings and arrays. The Array and String classes are monkey-patched with the module Getth.'
+	s.description = 'This gem defines methods first, second, third ... ninth, tenth for retrieving values from strings and arrays. The Array and String classes are monkey-patched with the module Getth.'
   s.authors = ["Sagar Pandya"]
   s.email = 'sarg@outlook.com'
   s.files = ["lib/getth.rb"]
--- test/test_getth.rb
+++ test/test_getth.rb
@@ -1,36 +1,22 @@
 #% cat test/test_getth.rb
 require 'minitest/autorun'
-require 'getth.rb'
+require 'getth'
 
 class GetthTest < Minitest::Test
-	def setup
-		@arr = ("a".."j").to_a
-		@str = "abcdefghij"
-	end
-	
-	def test_array
-		assert_equal "b", @arr.second
-		assert_equal "c", @arr.third
-		assert_equal "d", @arr.fourth
-		assert_equal "e", @arr.fifth
-		assert_equal "f", @arr.sixth
-		assert_equal "g", @arr.seventh
-		assert_equal "h", @arr.eighth
-		assert_equal "i", @arr.ninth
-		assert_equal "j", @arr.tenth
-	end
-	
-	
-	def test_string
-		assert_equal "b", @str.second
-		assert_equal "c", @str.third
-		assert_equal "d", @str.fourth
-		assert_equal "e", @str.fifth
-		assert_equal "f", @str.sixth
-		assert_equal "g", @str.seventh
-		assert_equal "h", @str.eighth
-		assert_equal "i", @str.ninth
-		assert_equal "j", @str.tenth
-	end
+  def setup
+    @arr = ("a".."j").to_a
+    @str = "abcdefghij"
+  end
 
+  def test_array
+    ("a".."j").each.with_index(1) do |e,i|
+      assert_equal e, @arr.send(i.ordinalize)
+    end
+  end
+
+  def test_string
+    ("a".."j").each.with_index(1) do |e,i|
+      assert_equal e, @str.send(i.ordinalize)
+    end
+  end
 end
