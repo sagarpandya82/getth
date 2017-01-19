8be05a552f547574c3732beae9c9161e5fad4f1b
Sagar Pandya <sarg@outlook.com>
2017-01-19 19:42:49 +0000

    Format README code; remove comments

--- lib/getth.rb
+++ lib/getth.rb
@@ -1,7 +1,6 @@
 require 'ordinalize'
 
 module Getth
-  #defines methods 'first', 'second', 'third'...'tenth' for retrieving values from strings/arrays
   (1..10).each do |i|
     define_method(i.ordinalize) { self[i.pred] }
   end
